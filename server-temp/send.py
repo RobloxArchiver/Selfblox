c6183f13f7fd4421450f6d03a62bc5fcdd080571
Uptight <RobloxArchiver@protonmail.com>
2022-07-14 14:21:30 -0400

    bye

--- server-temp/send.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def hello():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        await websocket.send("Uptight#0001 Networking Professional (Joke)")
-        await websocket.recv()
-
-asyncio.run(hello())
